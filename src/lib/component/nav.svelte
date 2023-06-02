6e220c77b54b6437549a16c27f95738d17b26adf
clement chong <clement.ch90@gmail.com>
2023-06-02 14:56:04 +0800

    .

--- src/lib/component/nav.svelte
+++ src/lib/component/nav.svelte
@@ -31,12 +31,6 @@
 		showAlert();
 		goto('/users/new/');
 	}
-
-	let isChecked = false;
-
-	toggleCheckboxes => {
-		isChecked = !isChecked;
-	}
 </script>
 
 <header class="mx-10 mt-5">
@@ -44,32 +38,32 @@
 		<div class="prose btn btn-ghost">
 			<label class="swap swap-flip text-2xl mr-2">
 				<!-- this hidden checkbox controls the state -->
-				<input type="checkbox" class="hidden" bind:checked={isChecked} disabled/>
-				<div class="swap-on">🐝</div>
-				<div class="swap-off">😈</div>
+				<input type="checkbox" class="hidden" />
+				<div data-set-theme="bumblebee" data-act-class="ACTIVECLASS">🐝</div>
+				<div data-set-theme="dracula" data-act-class="ACTIVECLASS">😈</div>
 			</label>
 			<!-- svelte-ignore a11y-click-events-have-key-events -->
 			<h1 class=" normal-case text-xl prose text-primary" on:click={goHome}>NEXT-JOBS</h1>
 		</div>
 		<div class="prose">
 			<label class="swap swap-rotate btn btn-ghost">
-				<input
-					type="checkbox"
-					data-toggle-theme="bumblebee,dracula"
-					data-act-class="ACTIVECLASS"
-					class="hidden"
-					bind:checked={isChecked}
-				/>
+				<input type="checkbox" class="hidden" />
+				<!-- sun icon -->
 				<svg
-					class="swap-on fill-current w-5 h-7 text-primary"
+					data-set-theme="bumblebee"
+					data-act-class="ACTIVECLASS"
+					class="swap-on fill-current w-10 h-10"
 					xmlns="http://www.w3.org/2000/svg"
-					viewBox="2 3 20 18"
+					viewBox="0 0 24 24"
 					><path
 						d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"
 					/></svg
 				>
+				<!-- moon icon -->
 				<svg
-					class="swap-off fill-current w-5 h-5 text-secondary"
+					data-set-theme="dracula"
+					data-act-class="ACTIVECLASS"
+					class="swap-off fill-current w-10 h-10"
 					xmlns="http://www.w3.org/2000/svg"
 					viewBox="0 0 24 24"
 					><path
